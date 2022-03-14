2e3bd51e7abec7a174deebceea5b2b6fdb72a672
Richard <krsw460@gmail.com>
2022-03-13 21:55:11 -0400

    added remote duel timer

--- script.js
+++ script.js
@@ -157,3 +157,33 @@ reset.addEventListener("click", () =>{
     display = 0;
     displayNum.textContent = display;
 })
+
+function countdown(elementName, minutes, seconds )
+{
+    let element, endTime, hours, mins, msLeft, time;
+
+    function twoDigits( n )
+    {
+        return (n <= 9 ? "0" + n : n);
+    }
+
+    function updateTimer()
+    {
+        msLeft = endTime - (+new Date);
+        if ( msLeft < 1000 ) {
+            element.innerHTML = "Time in round!";
+        } else {
+            time = new Date( msLeft );
+            hours = time.getUTCHours();
+            mins = time.getUTCMinutes();
+            element.innerHTML = (hours ? hours + ':' + twoDigits( mins ) : mins) + ':' + twoDigits( time.getUTCSeconds() );
+            setTimeout( updateTimer, time.getUTCMilliseconds() + 500 );
+        }
+    }
+
+    element = document.getElementById( elementName );
+    endTime = (+new Date) + 1000 * (60*minutes + seconds) + 500;
+    updateTimer();
+}
+
+countdown( "countdown", 50, 0 );
